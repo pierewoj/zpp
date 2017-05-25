ad0a80db86a31042a78e460a0e38bc5aa6d1af19
Adam Starak <starak.adam@gmail.com>
2017-05-25 12:58:56 +0200

    AlexNet on oxford-flowers-102

---
+++ notebooks/download.sh
@@ -0,0 +1,3 @@
+#Download manually: https://drive.google.com/uc?export=download&id=0B-hY4sQhIYO8dXBKekpKck8tVTQ"
+#wget http://www.cs.toronto.edu/~guerzhoy/tf_alexnet/bvlc_alexnet.npy
+tar -xvf flowers_dataset.tar
